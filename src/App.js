3b0910857988b8f54fbc13d95b66669b0f22aa5d
Fadhil Yusuf Rahadika <33994304+yusufrahadika@users.noreply.github.com>
2021-09-26 11:33:23 +0700

    Feature/test deployment (#4)
    * rename alias
    
    * Rename deployment action
    
    * Rename action file
    
    * Add !!
    
    * rename main

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <br />
-        <p>Hello human!</p>
+        <p>Hello human!!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
